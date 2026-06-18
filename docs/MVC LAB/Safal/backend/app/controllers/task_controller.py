40f2c2ccaf1ce8dfc679747b631a74296f7cbfb4
Safal Karki <isawfal@gmail.com>
2026-06-18 20:26:04 +0200

    updated docker compose

--- docs/MVC LAB/Safal/backend/app/controllers/task_controller.py
+++ docs/MVC LAB/Safal/backend/app/controllers/task_controller.py
@@ -3,17 +3,22 @@ from sqlalchemy.orm import Session
 
 from app.database import get_db
 from app. repositories.task_repository import TaskRepository
+from app. repositories.user_repository import UserRepository
+from app.services.task_service import TaskService, TaskNotFoundError, UserNotFoundError
 from app.schemas import Task, TaskCreate
-from app.services.task_service import TaskService, TaskNotFoundError
+# service = TaskService()
 
 router = APIRouter()
-# service = TaskService()
 
 def get_task_repo(db_session: Session = Depends(get_db)) -> TaskRepository:
     return TaskRepository(db_session)
 
-def get_task_service(repo: TaskRepository = Depends(get_task_repo)) -> TaskService:
-    return TaskService(repo)
+def get_user_repo(db_session: Session = Depends(get_db)) -> UserRepository:
+    return UserRepository(db_session)
+
+def get_task_service(tasks: TaskRepository = Depends(get_task_repo),
+                     users: UserRepository = Depends(get_user_repo)) -> TaskService:
+    return TaskService(tasks,users)
 
 @router.get("/", response_model=list[Task])
 def get_tasks(service: TaskService = Depends(get_task_service)) -> list[dict]:
@@ -24,11 +29,14 @@ def get_tasks(task_id: int, service: TaskService = Depends(get_task_service)) ->
     try:
         return service.get_task(task_id)
     except:
-        raise HTTPException(status_code=101, detail="Task not found")
+        raise HTTPException(status_code=404, detail="Task not found")
 
 @router.post("/", response_model=Task, status_code=201)
 def create_task(payload: TaskCreate, service: TaskService = Depends(get_task_service)):
-    return service.create_task(payload.title)
+    try:
+        return service.create_task(payload.title, payload.owner_id)
+    except UserNotFoundError:
+        raise HTTPException(status_code=404, detail="User not found")
 
 @router.delete("/{task_id}", status_code=204)
 def delete_task(task_id: int, service: TaskService = Depends(get_task_service)):
